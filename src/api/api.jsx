47a50981a6597fd586971755ee93354c1b1d6c6e
junesung1004 <junecoding1004@gmail.com>
2024-05-07 11:44:25 +0900

    api key = env파일로 관리하여 보안

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,9 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDHY6cAW86igOXLMgGCXUMsxxpiHnT8V0U",
-  authDomain: "pjs-shop-4f427.firebaseapp.com",
-  projectId: "pjs-shop-4f427",
-  storageBucket: "pjs-shop-4f427.appspot.com",
-  messagingSenderId: "745061480489",
-  appId: "1:745061480489:web:dc7e9efc6d6d40e3a14438",
-  measurementId: "G-BTFGK1H9P4"
+  apiKey: "NEXT_PUBLIC_FIREBASE_API_KEY",
+  authDomain: "NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
+  projectId: "NEXT_PUBLIC_FIREBASE_PROJECT_ID",
 };
