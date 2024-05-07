1e7ed218f53b558114fb3806532f9d345c45d792
junesung1004 <junecoding1004@gmail.com>
2024-05-07 11:37:49 +0900

    api키 업로드

---
+++ src/api/api.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHY6cAW86igOXLMgGCXUMsxxpiHnT8V0U",
+  authDomain: "pjs-shop-4f427.firebaseapp.com",
+  projectId: "pjs-shop-4f427",
+  storageBucket: "pjs-shop-4f427.appspot.com",
+  messagingSenderId: "745061480489",
+  appId: "1:745061480489:web:dc7e9efc6d6d40e3a14438",
+  measurementId: "G-BTFGK1H9P4"
+};
