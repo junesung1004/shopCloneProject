74b15411f3cfb72cf988545359273409a74c28ca
junesung1004 <junecoding1004@gmail.com>
2024-05-14 13:27:26 +0900

    <Image>  --> X   <img>  ok

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -33,7 +33,7 @@ export default function CartItem({product, idx}){
     <>
     <li>
       <p>{idx}</p>
-      <Image src={product.image} alt={product.title} width={100} height={100} />
+      <img src={product.image} alt={product.title}  />
       <p className="cartItemTitle">상품명 : {product.title}</p>
       <p className="cartItemOpt">옵션 :{product.option}</p>
       <div className="cartItemColor">
